6a1fc5d70f04cb91bd44875975c6b9ae7e77620a
zaynom <ouaziz0626@gmail.com>
2024-04-10 23:28:23 +0000

    task 2: Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
