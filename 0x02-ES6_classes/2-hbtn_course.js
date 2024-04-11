2628aace7b3828bee31315282c762f73e7540bb6
zaynom <ouaziz0626@gmail.com>
2024-04-11 20:11:47 +0000

    task 2: A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-underscore-dangle */
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = students;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
