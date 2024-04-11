2698c46fa6e0ffe5ce4064e82a2ae012975fae44
zaynom <ouaziz0626@gmail.com>
2024-04-11 20:23:46 +0000

    task 3: Methods, static methods, computed methods names... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
