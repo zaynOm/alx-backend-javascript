8f3016dbf29da161b7a0df6accca606b7c95054f
zaynom <ouaziz0626@gmail.com>
2024-04-11 03:04:10 +0000

    task 6: Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((values) => console.log(values));
+}
