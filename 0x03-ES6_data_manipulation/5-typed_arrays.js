2295d2e7643c22644b9d33a6ac0bcea9e7ef0d17
zaynom <ouaziz0626@gmail.com>
2024-04-15 18:06:58 +0000

    task 5: Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length - 1) throw new Error('Position outside range');
+
   const dv = new DataView(new ArrayBuffer(length));
   dv.setInt8(position, value);
   return dv;
