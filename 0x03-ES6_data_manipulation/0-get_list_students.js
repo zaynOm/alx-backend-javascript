27811617338be4f6f6c57d7e7170838bbef937fc
zaynom <ouaziz0626@gmail.com>
2024-04-15 06:10:17 +0000

    task 0: Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 export default function getListStudents() {
   return [
-    { id: 1, firstname: 'guillaume', location: 'san francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
