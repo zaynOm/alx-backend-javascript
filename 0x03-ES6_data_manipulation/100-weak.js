a80d116d8f49cae5b3ded34ee154b41e1e9c75ed
zaynom <ouaziz0626@gmail.com>
2024-04-16 11:08:42 +0000

    task 11: Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,6 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  let called = weakMap.get(endpoint) || 0;
-  called += 1;
+  let called = (weakMap.get(endpoint) || 0) + 1;
   if (called >= 5) throw new Error('Endpoint load is high');
-
   weakMap.set(endpoint, called);
 }
