11f190ec9671cd737098fb96e8d45a97500e2dc8
zaynom <ouaziz0626@gmail.com>
2024-04-16 11:19:00 +0000

    task 8: Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 export default function cleanSet(set, prefix) {
-  if (!set || !prefix || typeof set !== 'object' || typeof prefix !== 'string') return '';
-  return [...set]
+  if (!set || !prefix || typeof set !== 'object' || typeof prefix !== 'string') {
+    return '';
+  }
+  return Array.from(set)
     .filter((item) => item.startsWith(prefix))
     .map((item) => item.slice(prefix.length))
     .join('-');
