2024d6271ec92b0e6faacc1f5b045db7ac3638d4
zaynom <ouaziz0626@gmail.com>
2024-06-06 09:06:21 +0100

    Task 1: Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
