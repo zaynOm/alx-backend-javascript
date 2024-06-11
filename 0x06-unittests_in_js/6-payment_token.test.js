709a7eb2962d0c6f67ded3acaecb023f062cc568
zaynom <ouaziz0626@gmail.com>
2024-06-11 13:02:01 +0100

    Task 6: Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with a Successful response when true is passed', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  it('should return a resolved promise with undefined when false is passed', function(done) {
+    getPaymentTokenFromAPI(false)
+      .then((res) => {
+        expect(res).to.be.undefined;
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
