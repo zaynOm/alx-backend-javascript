5f516d66a085ec9745d5560835b187a79ff7f9a3
zaynom <ouaziz0626@gmail.com>
2024-06-19 21:24:08 +0100

    full server implementation

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
