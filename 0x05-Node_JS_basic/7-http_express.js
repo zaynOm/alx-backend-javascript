130a38f81716e122d1708826f0c1977fe70a22b7
zaynom <ouaziz0626@gmail.com>
2024-08-21 01:11:07 +0100

    Task 7: Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const fs = require('fs');
 
-const app = express();
 const port = 1245;
+const app = express();
 
 function countStudents(path) {
-  return new Promise((resolve, rejects) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) return rejects(Error('Cannot load the database'));
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
 
-      const students = data.split('\n');
-      students.shift();
+      const result = [];
+      const lines = data.split('\n').filter((line) => line);
+      result.push(`Number of students: ${lines.length - 1}`);
+      const students = lines.slice(1).map((line) => line.split(','));
 
       const fields = {};
-      students.forEach((row) => {
-        const cols = row.split(',');
-        fields[cols[3]] = [...(fields[cols[3]] || []), cols[0]];
+      students.forEach((student) => {
+        if (!fields[student[3]]) fields[student[3]] = [];
+        fields[student[3]].push(student[0]);
       });
 
-      const res = [`Number of students: ${students.length}`];
-      for (const [key, value] of Object.entries(fields)) {
-        res.push(
-          `Number of students in ${key} ${value.length}. List: ${value.join(', ')}`,
-        );
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          result.push(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+          );
+        }
       }
-      resolve(res.join('\n'));
+
+      resolve(result.join('\n'));
     });
   });
 }
@@ -34,13 +41,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   countStudents(process.argv[2])
     .then((data) => {
+      res.setHeader('Content-Type', 'text/plain');
       res.send(`This is the list of our students\n${data}`);
     })
-    .catch((err) => {
-      res.end(`This is the list of our students\n${err.message}`);
+    .catch((error) => {
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${error.message}`);
     });
 });
 
