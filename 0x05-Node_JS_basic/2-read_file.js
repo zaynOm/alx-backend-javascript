34f8a615ce05a23dff4bcab2ec6829aec2571ca6
zaynom <ouaziz0626@gmail.com>
2024-08-21 01:37:45 +0100

    Task 2: Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,24 +4,28 @@ function countStudents(path) {
   let data;
   try {
     data = fs.readFileSync(path, 'utf8');
-  } catch (error) {
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
-
   const lines = data.split('\n').filter((line) => line);
-  lines.shift();
+  console.log(`Number of students: ${lines.length - 1}`);
+  const students = lines.slice(1).map((line) => line.split(','));
 
   const fields = {};
-  lines.forEach((row) => {
-    const cols = row.split(',');
-    fields[cols[3]] = [...(fields[cols[3]] || []), cols[0]];
-  });
+  for (const student of students) {
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(student[0]);
+  }
 
-  console.log('Number of students:', lines.length);
-  for (const [field, names] of Object.entries(fields)) {
-    console.log(
-      `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
-    );
+  for (const field in fields) {
+    if (Object.prototype.hasOwnProperty.call(fields, field)) {
+      console.log(
+        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+      );
+    }
   }
 }
 
