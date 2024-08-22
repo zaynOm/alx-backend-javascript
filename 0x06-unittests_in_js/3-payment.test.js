6783c8624f96be4ca2e15652c7b2aa6af21f4d4c
zaynom <ouaziz0626@gmail.com>
2024-08-23 00:33:16 +0100

    Task 3: Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,8 +4,17 @@ const sendPaymentRequestToApi = require("./3-payment");
 const { expect } = require("chai");
 
 describe("sendPaymentRequestToApi", function () {
+  let spy;
+
+  this.beforeEach(() => {
+    spy = Sinon.spy(Utils, "calculateNumber");
+  });
+
+  this.afterEach(() => {
+    spy.restore();
+  });
+
   it('Should call Utils.calculateNumber with "SUM", 100 and 20', function () {
-    const spy = Sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnceWithExactly("SUM", 100, 20)).to.be.true;
     spy.restore();
