4ab2b7d725011e2cfcae4731f69839da06ea846a
zaynom <ouaziz0626@gmail.com>
2024-08-22 19:36:37 +0100

    Task 0: Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,6 +2,10 @@ var assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
+  it("Should return 0, when a = 0 and b = 0", function () {
+    assert.equal(calculateNumber(0, 0), 0);
+  });
+
   it("Should return 2, when a = 1.6 and b = 0", function () {
     assert.equal(calculateNumber(1.6, 0), 2);
   });
@@ -20,5 +24,13 @@ describe("calculateNumber", function () {
 
   it("Should return 6, when a = 1.5 and b = 3.7", function () {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it("should return -5 when a = -2.7 and b = -2.2", function () {
+    assert.equal(calculateNumber(-2.7, -2.2), -5);
+  });
+
+  it("should return -7 when a = -7.7 and b = 1.2", function () {
+    assert.equal(calculateNumber(-7.7, 1.2), -7);
   });
 });
