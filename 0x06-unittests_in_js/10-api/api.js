68ca2b3d75fbd1f29d3f06adab970ca51d2b5cf8
zaynom <ouaziz0626@gmail.com>
2024-08-23 02:34:19 +0100

    Task 10: Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
 const app = express();
+const port = 7865;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.end("Welcome to the payment system");
+  res.send("Welcome to the payment system");
 });
 
 app.get("/cart/:id([0-9]+)", (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get("/available_payments", (req, res) => {
@@ -15,9 +18,11 @@ app.get("/available_payments", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.send(`Welcome ${req.body.username}`);
+  res.send(`Welcome ${req.body.userName}`);
 });
 
-app.listen(7865, () => {
+app.listen(port, () => {
   console.log("API available on localhost port 7865");
 });
+
+module.exports = app;
